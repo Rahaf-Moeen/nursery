c9508f372d13e8f8647d7b78a387d271b7a5ae8d
Moaz A.Moshtha <moazmoshtha@gmail.com>
2023-05-31 17:54:58 +0300

    add alter components

---
+++ app/View/Components/Alert.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Alert extends Component
+{
+    public string $message;
+    public string $title;
+    public string $type;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($message = null, $title = null, $type = null)
+    {
+        $this->message = $message;
+        $this->title = $title;
+        $this->type = $type;
+    }
+
+    public function backgroundCSS(): string
+    {
+        return [
+            'error' => 'bg-red-500',
+            'success' => 'bg-emerald-500',
+            'warning' => 'bg-yellow-400'
+        ][$this->type];
+    }
+
+    public function textCss(): string
+    {
+        return [
+            'error' => 'text-red-500 dark:text-red-400',
+            'success' => 'text-emerald-500 dark:text-emerald-400',
+            'warning' => 'text-yellow-400 dark:text-yellow-300'
+        ][$this->type];
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.alert');
+    }
+}
