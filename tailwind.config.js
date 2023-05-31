a2e18f23cd8e80ec68352f8d72797f41d33830f2
Moaz A.Moshtha <moazmoshtha@gmail.com>
2023-05-31 17:50:27 +0300

    add Cairo font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import rtl from 'tailwindcss-rtl';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: "class",
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -12,10 +14,13 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ['Cairo'],
             },
         },
     },
 
-    plugins: [forms],
+    plugins: [
+        forms,
+        rtl,
+    ],
 };
