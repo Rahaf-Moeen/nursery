9d916ea7ff0fcbbeacfb28d8837208d35fe57b0b
Moaz A.Moshtha <moazmoshtha@gmail.com>
2023-06-01 16:35:13 +0300

    create user for each role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -11,13 +13,35 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory()->create([
+        $admin_role = Role::create(['name' => 'admin']);
+        $parent_role = Role::create(['name' => 'parent']);
+        $baby_sitter_role = Role::create(['name' => 'babysitter']);
+        $accountant_role = Role::create(['name' => 'accountant']);
+
+        $admin = User::factory()->create([
             'national_id' => '123456789',
             'username' => '123456789',
             'email' => 'admin@nursery.com',
-            'password' => bcrypt('password'),
             'is_employee' => true,
         ]);
+        $admin->assignRole($admin_role);
+
+        $parent = User::factory()->create([
+            'email' => 'parent@nursery.com',
+        ]);
+        $parent->assignRole($parent_role);
+
+        $babysitter = User::factory()->create([
+            'email' => 'babysitter@nursery.com',
+            'is_employee' => true
+        ]);
+        $babysitter->assignRole($baby_sitter_role);
+
+        $accountant = User::factory()->create([
+            'email' => 'accountant@nursery.com',
+            'is_employee' => true
+        ]);
+        $accountant->assignRole($accountant_role);
     }
 
 }
